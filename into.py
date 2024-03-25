ed62b73632965fb792db846a158f0fdcda0fd648
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2024-03-25 22:34:59 +0100

    working dir

---
+++ into.py
@@ -0,0 +1,12 @@
+# create variable name o hold hello world
+message = """hEllo world to Nkiru's world of world  """
+# print(len(message))
+# print(len(message))
+
+# print(message[6:])
+# print(message.find('world'))
+# print(message.count('l'))
+
+# print(message.replace('world', 'kenya'))
+# or
+new_message = message.replace
