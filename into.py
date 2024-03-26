f8e6102d32b4ff15f1d08affdda15c95ee8e7eb6
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2024-03-27 00:34:02 +0100

    update

--- into.py
+++ into.py
@@ -24,6 +24,33 @@
 # num = 1
 # num *= 10
 # print(num)
-num_1 = 3
-num_2 = 2
-print (num_1 < num_2)
+# num_1 = 3
+# num_2 = 2
+# print (num_1 < num_2# )
+# num_1 = '100'
+# num_2 = '200'
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+# print(num_1 + num_2)
+
+# courses.append('Art')
+# courses.append('Economics')
+# courses.insert(0, 'Art')
+# courses.insert(2, 'geology')
+#
+# courses_2 = ['igbo' ,'hausa']
+# courses.extend(courses_2)
+# courses.remove('Math') math removed from course list
+# pop also a way to remove values
+courses = ['History', 'Math', 'Physics', 'CompSci']
+# popped = courses.pop() #pop method will remove the last item
+# wanna reverse our list?
+# courses.reverse()
+# wanna sort our list?
+# courses.sort() # will sort it alphabetical order
+nums = [1,2,3,4,5,6]
+nums.sort(reverse=True)
+courses.sort(reverse=True)
+print(courses)
+print(nums)
+# print(popped)
